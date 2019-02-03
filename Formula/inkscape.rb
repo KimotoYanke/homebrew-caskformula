7af5845938b9164f72f57034788dc3271acc60bc
KimotoYanke <kimotoyanke@gmail.com>
2019-02-03 19:21:56 +0900

    Update inkscape.rb

--- Formula/inkscape.rb
+++ Formula/inkscape.rb
@@ -2,7 +2,6 @@ class Inkscape < Formula
   desc "Professional vector graphics editor"
   homepage "https://inkscape.org/"
   url "https://gitlab.com/api/v4/projects/inkscape%2Finkscape/repository/archive.tar.bz2"
-  sha256 "b08fbeb9beda8c46279ef34a59212904a043195262a10fd6098ec6ba756594d5"
 
   depends_on "automake" => :build
   depends_on "cmake" => :build
